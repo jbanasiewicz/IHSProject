3349809f3b40fcace556c6789b556ee28a148f3b
jbanasiewicz <jbanasiewicz92@gmail.com>
2017-09-27 14:27:40 +0200

    JAXB converter added

--- src/main/java/isbnScan/Scanner.java
+++ src/main/java/isbnScan/Scanner.java
@@ -2,14 +2,19 @@ package isbnScan;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.xml.SimpleXmlHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.*;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 
 @Component
+@EnableWebMvc
+@Configuration
 public class Scanner {
 
     @Autowired
@@ -24,18 +29,19 @@ public class Scanner {
         bookTitle = "harry potter i czara ognia";
         bookTitle = bookTitle.replaceAll(" ", "+");
         String url = "http://www.isbndb.com/api/books.xml?access_key=4N35Z5BX&index1=title&value1=" + bookTitle;
-        /*HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-        ResponseEntity<String> response = restTemplate().exchange(url, HttpMethod.GET, entity, String.class);
-        String responseBody = response.getBody();*/
+        //ResponseEntity<String> response = restTemplate().exchange(url, HttpMethod.GET, entity, String.class);
+        //String responseBody = response.getBody();*/
 
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
+        restTemplate.getMessageConverters().add(new Jaxb2RootElementHttpMessageConverter());
+        ResponseEntity<ISBNdb> bookInfo = restTemplate().exchange(url, HttpMethod.GET, entity, ISBNdb.class);
+        ISBNdb bookInfoBody = bookInfo.getBody();
+        //ISBNdb bookInfo = restTemplate.getForObject(url, ISBNdb.class);
 
-        ISBNdb bookInfo = restTemplate.getForObject(url, ISBNdb.class);
-
-        return bookInfo;
+        return bookInfoBody;
     }
 }
