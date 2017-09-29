e98c992841ff1086ce4683f2200b9bd0fb66f870
jbanasiewicz <jbanasiewicz92@gmail.com>
2017-09-29 08:57:28 +0200

    code refactor

--- src/main/java/isbnScan/AmazonSearch.java
+++ src/main/java/isbnScan/AmazonSearch.java
@@ -23,62 +23,65 @@ public class AmazonSearch {
             try {
                 List<WebElement> buttons = driver.findElements(By.cssSelector(".a-button.a-spacing-mini.a-button-toggle.format"));
                 for (int i = 0; i < buttons.size(); i++) {
-                    try {
-                        String button = buttons.get(i).getText();
-                        if (button.toLowerCase().contains("paperback")) {
-                            if (button.toLowerCase().contains("$")) {
-                                if (button.toLowerCase().contains("from")) {
-                                    if (button.toLowerCase().contains("perfect")) {
-                                        amazonPrice = Double.parseDouble(button.replace("Perfect Paperback\n" + " from $", "").replace(",", ""));
-                                        driver.quit();
-                                        return priceConverter(amazonPrice);
-                                    } else {
-                                        amazonPrice = Double.parseDouble(button.replace("Paperback\n" + " from $", "").replace(",", ""));
-                                        driver.quit();
-                                        return priceConverter(amazonPrice);
-                                    }
+                    String button = buttons.get(i).getText();
+                    if (button.toLowerCase().contains("paperback")) {
+                        if (button.toLowerCase().contains("$")) {
+                            if (button.toLowerCase().contains("from")) {
+                                if (button.toLowerCase().contains("perfect")) {
+                                    amazonPrice = Double.parseDouble(button.replace("Perfect Paperback\n" + " from $", "").replace(",", ""));
+                                    driver.quit();
+                                    return convertPrice(amazonPrice);
                                 } else {
-                                    if (button.toLowerCase().contains("perfect")) {
-                                        amazonPrice = Double.parseDouble(button.replace("Perfect Paperback\n" + " $", "").replace(",", ""));
-                                        driver.quit();
-                                        return priceConverter(amazonPrice);
-                                    } else {
-                                        amazonPrice = Double.parseDouble(button.replace("Paperback\n" + " $", "").replace(",", ""));
-                                        driver.quit();
-                                        return priceConverter(amazonPrice);
-                                    }
+                                    amazonPrice = Double.parseDouble(button.replace("Paperback\n" + " from $", "").replace(",", ""));
+                                    driver.quit();
+                                    return convertPrice(amazonPrice);
+                                }
+                            } else {
+                                if (button.toLowerCase().contains("perfect")) {
+                                    amazonPrice = Double.parseDouble(button.replace("Perfect Paperback\n" + " $", "").replace(",", ""));
+                                    driver.quit();
+                                    return convertPrice(amazonPrice);
+                                } else {
+                                    amazonPrice = Double.parseDouble(button.replace("Paperback\n" + " $", "").replace(",", ""));
+                                    driver.quit();
+                                    return convertPrice(amazonPrice);
                                 }
                             }
                         }
-                    } catch (NoSuchElementException a) {
-                        driver.quit();
-                        return 0.00;
                     }
+
                 }
                 driver.quit();
                 return 0.00;
             } catch (NoSuchElementException b) {
                 driver.quit();
+                System.out.println("Could not find buttons with prices");
                 return 0.00;
             }
         } catch (NoSuchElementException b) {
             driver.quit();
+            System.out.println("Could not find title with link");
             return 0.00;
         }
     }
 
-    public double priceConverter(double priceUSDEUR) {
+    public double convertPrice(double priceUSDEUR) {
         double pricePLN = 0.00;
         WebDriver driver = new HtmlUnitDriver();
         driver.get("https://stooq.pl/q/?s=usdpln");
-        WebElement usdPln = driver.findElement(By.id("aq_usdpln_c5"));
-        DecimalFormat formatter = new DecimalFormat("0.00");
-        String a  = usdPln.getText();
-        double valueUSDPLN = Double.parseDouble(a);
-        String b = formatter.format(valueUSDPLN * priceUSDEUR).replace(',', '.');
-        pricePLN = Double.parseDouble(b.replace(",", ""));
-
-        driver.quit();
-        return pricePLN;
+        try {
+            WebElement usdPln = driver.findElement(By.id("aq_usdpln_c5"));
+            DecimalFormat formatter = new DecimalFormat("0.00");
+            String a  = usdPln.getText();
+            double valueUSDPLN = Double.parseDouble(a);
+            String b = formatter.format(valueUSDPLN * priceUSDEUR).replace(',', '.');
+            pricePLN = Double.parseDouble(b.replace(",", ""));
+            driver.quit();
+            return pricePLN;
+        } catch (NoSuchElementException b) {
+            driver.quit();
+            System.out.println("Error in finding USDPLN");
+            return 0.00;
+        }
     }
 }
